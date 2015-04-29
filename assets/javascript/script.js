a01aa2d3afcb7935ceba639d893a7ba07b1f0cc2
Zhipan1 <Zhilingpan@hotmail.com>
2015-04-28 23:35:33 -0700

    added in sponsors

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,40 +1,97 @@
 $(document).ready(function() {
 
   var window_height = $(window).height();
-  console.log(window_height)
   //adjust section height
   $(".section.full").css({"height": window_height});
 
-  $("#nav-menu").click(function() {
+  $(".nav-menu").click(function() {
     $("#nav-sections").show()
     setTimeout(function() {
       $("#nav-sections").addClass("active")
     }, 50)
   });
 
-  $("#nav-close").click(function() {
-    $("#nav-sections").removeClass("active")
-    setTimeout(function() {
-      $("#nav-sections").hide()
-    }, 300)
+
+  if ($(window).width() > 480) {
+    $(window).scroll(switchNav);
+    $("body").backstretch("assets/img/crowd.jpg");
+    $("#apply").backstretch("assets/img/stars.jpeg");
+  } else {
+    $(window).scroll(switchNavMobile);
+     $("#nav-close, .nav-section").click(function() {
+      $("#nav-sections").removeClass("active")
+      setTimeout(function() {
+        $("#nav-sections").hide()
+      }, 300)
+    });
+  }
+
+  $(document).on("scroll", onScroll);
+
+  //smoothscroll
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function () {
+        $(this).parent().removeClass('active');
+    })
+    $(this).parent().addClass('active');
+
+    var target = this.hash,
+        menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top+2
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+    });
   });
-
-  sponsors = ["a16z.svg", "facebook.svg", "leapmotion.svg", "redbull.svg", "accel.svg", "firebase.svg", "lob.svg", "sendgrid.svg", "aerospike.svg", "fluxcoffee.svg", "lsvp.svg", "sep.svg", "aws.svg", "ford.svg", "mashery.svg", "socialapps.svg", "bee.svg", "foundry.png", "medallia.svg", "stab.svg", "bloomberg.svg", "gap.svg", "meraki.svg", "stackoverflow.svg", "braintree.svg", "gogosqueez.svg", "mgwu.png", "storm8.svg", "cambrian.svg", "google.svg", "microsoft.svg", "teespring.svg", "castlight.svg", "gracenote.svg", "mlh.svg", "texasinstruments.svg", "citris.svg", "hackers.svg", "mongodb.svg", "thalmic.svg", "codeship.svg", "hired.svg", "moxtra.png", "thiel.svg", "conviva.svg", "inventionlab.png", "namecheap.png", "thinksys.svg", "crunchfund.svg", "jump.png", "nest.svg", "typesafe.svg", "dodocase.png", "kairos.svg", "oculus.png", "uber.svg", "draperu.svg", "kloudless.svg", "ordrin.png", "unity.svg", "dropbox.svg", "kpcb.svg", "paloalto.svg", "witai.svg", "ebay.svg", "leadgenius.svg", "quixey.svg", "ziggeo.svg"]
-
-  //too lazy to write html for the 40 sponsors
-  var appendSponsor = function(path) {
-    var img = $("<img class='sponsor'></img>")
-    img.attr("src", "assets/img/sponsors/" + path)
-    $("#sponsors-container").append(img)
-  }
-
-  var appendSponsors = function(sponsors) {
-    for (i in sponsors) {
-      appendSponsor(sponsors[i]);
-    }
-  }
-
-  appendSponsors(sponsors);
-
-
 });
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('#nav-sections a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('#nav-sections .nav-section').removeClass("active");
+          currLink.parent().addClass("active");
+      }
+      else{
+          currLink.parent().removeClass("active");
+      }
+  });
+}
+
+
+var switchNav = function() {
+  if ($(window).scrollTop() > 15) {
+    $("#nav").addClass("active");
+    $("#logo_white").hide();
+    $("#logo_orange").show();
+  } else {
+    $("#nav").removeClass("active");
+    $("#logo_white").show();
+    $("#logo_orange").hide();
+  }
+}
+
+var switchNavMobile = function() {
+  if ($(window).scrollTop() > 15) {
+    $("#nav").addClass("active-mobile");
+    $("#logo_white").hide();
+    $("#logo_orange").show();
+  } else {
+    $("#nav").removeClass("active-mobile");
+    $("#logo_white").show();
+    $("#logo_orange").hide();
+  }
+}
+
+var activateSection = function(section) {
+  current = $(".nav-section.active");
+  current.removeClass("active");
+  $(section).addClass("active")
+}
