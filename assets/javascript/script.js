a797ce808ae36d376edd156ff5e340207a93e497
Kevin Ji <kevin.ji@outlook.com>
2015-08-12 12:02:05 -0700

    script: Clean up using JSCS rules

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,169 +1,170 @@
 $(document).ready(function() {
-
   logEmoji();
 
   //var window_height = $(window).height();
   //adjust section height
 
   $(".nav-menu").click(function() {
-    $("#nav-sections").show()
+    $("#nav-sections").show();
     setTimeout(function() {
-      $("#nav-sections").addClass("active")
-    }, 50)
+      $("#nav-sections").addClass("active");
+    }, 50);
   });
-
 
   if ($(window).width() > 480) {
     $(window).scroll(switchNav);
     $("body").backstretch("assets/img/hackathon_background.jpg");
     $(document).on("scroll", onScroll);
-    $(".section").click(loadPixelOnMouse)
+    $(".section").click(loadPixelOnMouse);
   } else {
     $("#nav").addClass("active-mobile");
     $("#logo_white").hide();
     $("#logo_orange").show();
-     $("#nav-close, .nav-section").click(function() {
-      $("#nav-sections").removeClass("active")
+    $("#nav-close, .nav-section").click(function() {
+      $("#nav-sections").removeClass("active");
       setTimeout(function() {
-        $("#nav-sections").hide()
-      }, 300)
+        $("#nav-sections").hide();
+      }, 300);
     });
   }
 
   $(".drake-me").click(function() {
-    var target = $('#drake-here')[0]
-    drakeMe(target)
-    $(".section").unbind('click', loadPixelOnMouse)
-    $(".section").click(drakeOnMouse)
-    everythingDrake()
-  })
-
+    var target = $("#drake-here")[0];
+    drakeMe(target);
+    $(".section").unbind("click", loadPixelOnMouse);
+    $(".section").on("click", drakeOnMouse);
+    everythingDrake();
+  });
 
   //smoothscroll
-  $('a[href^="#"]').on('click', function (e) {
+  $("a[href^='#']").on("click", function(e) {
     e.preventDefault();
     $(document).off("scroll");
 
-    $('a').each(function () {
-        $(this).parent().removeClass('active');
-    })
-    $(this).parent().addClass('active');
+    $("a").each(function() {
+      $(this).parent().removeClass("active");
+    });
+    $(this).parent().addClass("active");
 
-    var target = this.hash,
-        menu = target;
+    var target = this.hash;
+    var menu = target;
     $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top+2
-    }, 500, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
+    $("html, body").stop().animate({
+      "scrollTop": $target.offset().top + 2
+    }, 500, "swing", function() {
+      window.location.hash = target;
+      $(document).on("scroll", onScroll);
     });
   });
 });
 
-loadImg('.partner')
-loadImg('.sponsor')
+loadImg(".partner");
+loadImg(".sponsor");
 
-var pixels = ["ambulance.svg", "battery-half.svg", "chevron-left.svg", "chevron-right.svg", "diamond.svg", "emoticon-confused.svg", "file-text.svg", "food.svg", "hand.svg", "location.svg", "palette.svg", "question.svg", "ruler-triangle.svg", "stats-down.svg", "tshirt.svg"];
+var pixels = ["ambulance.svg", "battery-half.svg", "chevron-left.svg",
+  "chevron-right.svg", "diamond.svg", "emoticon-confused.svg", "file-text.svg",
+  "food.svg", "hand.svg", "location.svg", "palette.svg", "question.svg",
+  "ruler-triangle.svg", "stats-down.svg", "tshirt.svg"];
 
 function randomPixel() {
   var rand = Math.floor(Math.random() * pixels.length);
   var path = "assets/img/pixel/" + pixels[rand];
-  var html = '<img class="pixel" src="' + path + '" />'
-  return $(html)
+  var html = "<img class='pixel' src='" + path + "'";
+  return $(html);
 }
 
 function loadPixels(num, container) {
-  var $container = $(container)
+  var $container = $(container);
   for (var i = 0; i < num; i++) {
-    var pixel = randomPixel()
+    var pixel = randomPixel();
     $container.append(pixel);
-    return pixel
+    return pixel;
   }
 }
 
-
 function loadPixelOnMouse(e) {
   var $container = $(this);
-  var offset = $container.offset()
-  var pixel = loadPixels(1, $container)
+  var offset = $container.offset();
+  var pixel = loadPixels(1, $container);
   pixel.css({
     left: e.pageX - offset.left,
     top: e.pageY - offset.top
-  })
+  });
 }
 
 function loadDrake() {
-  var path = "assets/img/drake.png"
-  var html = '<img class="drake" src="' + path + '" />'
-  return $(html)
+  var path = "assets/img/drake.png";
+  var html = "<img class='drake' src='" + path + "'>";
+  return $(html);
 }
 
 function drakeOnMouse(e) {
   var $container = $(this);
-  var offset = $container.offset()
-  var drake = loadDrake()
-  $container.append(drake)
+  var offset = $container.offset();
+  var drake = loadDrake();
+  $container.append(drake);
   drake.css({
     left: e.pageX - offset.left,
     top: e.pageY - offset.top
-  })
+  });
 }
 
 function everythingDrake() {
-  $(".section:not(#footer) img").attr('src', "assets/img/drake.png")
+  $(".section:not(#footer) img").attr("src", "assets/img/drake.png");
 }
 
-
-function onScroll(event){
+function onScroll(event) {
   var scrollPos = $(document).scrollTop();
-  $('#nav-sections a').each(function () {
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
-      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-          $('#nav-sections .nav-section').removeClass("active");
-          currLink.parent().addClass("active");
-      }
-      else{
-          currLink.parent().removeClass("active");
-      }
+  $("#nav-sections a").each(function() {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top <= scrollPos &&
+        refElement.position().top + refElement.height() > scrollPos) {
+      $("#nav-sections .nav-section").removeClass("active");
+      currLink.parent().addClass("active");
+    } else {
+      currLink.parent().removeClass("active");
+    }
   });
 }
 
 SC.initialize({
+  // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
   // too lazy to hide this server side
-  client_id: "5bf5997727498f138cd393324936657c",
+  client_id: "5bf5997727498f138cd393324936657c"
+  // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
 });
 
 function logSongs(options) {
-  SC.get('/tracks', options, function(tracks) {
+  SC.get("/tracks", options, function(tracks) {
     tracks.map(function(track) {
-      console.log(track.title, track.bpm, track)
-    })
-  })
+      console.log(track.title, track.bpm, track);
+    });
+  });
 }
 
-function drakeMe(target, too_late) {
-  var link = '/tracks'
-  var search = 'octobersveryown'
+function drakeMe(target, tooLate) {
+  var link = "/tracks";
+  var search = "octobersveryown";
   SC.get(link, {
     q: search,
     limit: 25
   }, function(tracks) {
-    console.log(target)
-    var song = tracks[Math.floor((tracks.length - 1) * Math.random())]
-    SC.oEmbed(song.uri, { auto_play: true }, target)
-  })
+    console.log(target);
+    var song = tracks[Math.floor((tracks.length - 1) * Math.random())];
+    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+    SC.oEmbed(song.uri, {auto_play: true}, target);
+    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+  });
 }
 
 function loadImg(selector) {
   $(selector).each(function(index, sponsor) {
-    var $sponsor = $(sponsor)
-    var src = $sponsor.attr('data-src')
-    $sponsor.attr('src', src)
-  })
+    var $sponsor = $(sponsor);
+    var src = $sponsor.attr("data-src");
+    $sponsor.attr("src", src);
+  });
 }
-
 
 var switchNav = function() {
   if ($(window).scrollTop() > 15) {
@@ -175,7 +176,7 @@ var switchNav = function() {
     $("#logo_white").show();
     $("#logo_orange").hide();
   }
-}
+};
 
 var switchNavMobile = function() {
   if ($(window).scrollTop() > 15) {
@@ -187,25 +188,49 @@ var switchNavMobile = function() {
     $("#logo_white").show();
     $("#logo_orange").hide();
   }
-}
+};
 
 var activateSection = function(section) {
   current = $(".nav-section.active");
   current.removeClass("active");
-  $(section).addClass("active")
-}
+  $(section).addClass("active");
+};
 
-var poem = "%cCal Hacks, a poem %c  \n\n%cThere once was a wee young lad,\nwhose brain had gone all sad,\nfor he didn't ignite his thoughts or his bytes,\nand now his friends laugh cause his life isn't rad.\nSo one dark, stormy night,\nhe found, to his delight,\na hackathon so, extravagant, bold,\nthat he giggled with glee as he hit apply.\nAnd now with his acceptance,\nhis friends are filled with reverence,\nthey pleaded and pleaded, for a chance to compete with,\nthis lad in Cal Hacks and go undefeated.\n\nThere once was a bored little lass,\nwho wanted to try out SASS,\nso she hit apply and overnight,\nshe found her life filled with delight.\nNow everywhere she go,\nShe radiates a glow,\ncause she's full of ideas of uses and features,\nfor all types of users and all types of creatures.\n\nAnd now for the final guest,\na hacker with wonder and zest,\nwho doesn't deplore a chance to explore,\ncome join us for 2 days of hacking galore!"
+var poem = "%cCal Hacks, a poem %c\n\n" +
+
+  "%cThere once was a wee young lad,\n" +
+  "whose brain had gone all sad,\n" +
+  "for he didn't ignite his thoughts or his bytes,\n" +
+  "and now his friends laugh cause his life isn't rad.\n" +
+  "So one dark, stormy night,\n he found, to his delight,\n" +
+  "a hackathon so, extravagant, bold,\n" +
+  "that he giggled with glee as he hit apply.\n" +
+  "And now with his acceptance,\n his friends are filled with reverence,\n" +
+  "they pleaded and pleaded, for a chance to compete with,\n" +
+  "this lad in Cal Hacks and go undefeated.\n\n" +
+
+  "There once was a bored little lass,\n who wanted to try out SASS,\n" +
+  "so she hit apply and overnight,\n" +
+  "she found her life filled with delight.\n" +
+  "Now everywhere she go,\n she radiates a glow,\n" +
+  "cause she's full of ideas of uses and features,\n" +
+  "for all types of users and all types of creatures.\n\n" +
+
+  "And now for the final guest,\n a hacker with wonder and zest,\n" +
+  "who doesn't deplore a chance to explore,\n" +
+  "come join us for 2 days of hacking galore!";
 
 var logEmoji = function() {
   var styles = {
     please: "color: #336699; font-weight: bold",
     emoji: function() {
-      return "background-image: url('http://emojipedia.org/wp-content/uploads/2013/07/6-winking-face.png'); background-size: cover";
+      return "background-image: url('" +
+        "http://emojipedia.org/wp-content/uploads/2013/07/6-winking-face.png" +
+        "'); background-size: cover";
     }
   };
   console.log(
     poem,
     styles.please, styles.emoji(), styles.please
   );
-}
+};
